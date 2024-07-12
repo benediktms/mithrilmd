a24abcfca8a21790f1de60cf1a2c4baa3f538ff3
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-12 22:28:32 +0100

    chore: clean up boilerplate

--- apps/client/svelte.config.js
+++ apps/client/svelte.config.js
@@ -4,17 +4,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			'@browser-package': '../../packages/browser-package/src/index.ts',
-			$lib: 'src/lib',
-		},
-	},
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@browser-package': '../../packages/browser-package/src/index.ts',
+      $lib: 'src/lib'
+    }
+  }
 };
 
 export default config;
