47baa8bc36a7205c7b708d3acf946b1030a24c83
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-15 22:20:33 +0100

    feat: bulk add primitives

--- packages/shadcn-svelte-ui-primitives/src/lib/index.ts
+++ packages/shadcn-svelte-ui-primitives/src/lib/index.ts
@@ -13,3 +13,57 @@ export {
   CardTitle
 } from './components/ui/card/index.js';
 export type { HeadingLevel } from './components/ui/card/index.js';
+
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from './components/ui/collapsible/index.js';
+
+export {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandLoading,
+  CommandSeparator,
+  CommandShortcut
+} from './components/ui/command/index.js';
+
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger
+} from './components/ui/dialog/index.js';
+
+export { Input } from './components/ui/input/index.js';
+export type { FormInputEvent, InputEvents } from './components/ui/input/index.js';
+
+export { Label } from './components/ui/label/index.js';
+
+export {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger
+} from './components/ui/popover/index.js';
+
+export {
+  ResizableHandle,
+  ResizablePane,
+  ResizablePaneGroup
+} from './components/ui/resizable/index.js';
+
+export { ScrollArea, ScrollAreaScrollbar } from './components/ui/scroll-area/index.js';
+
+export { Separator } from './components/ui/separator/index.js';
