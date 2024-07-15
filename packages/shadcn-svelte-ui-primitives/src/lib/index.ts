50874ad74a53238209e6ee0e94e4af348d78320b
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-15 22:03:19 +0100

    feat: add card primitive

--- packages/shadcn-svelte-ui-primitives/src/lib/index.ts
+++ packages/shadcn-svelte-ui-primitives/src/lib/index.ts
@@ -1,5 +1,15 @@
-export { Button, Root, buttonVariants } from './components/ui/button/index.js';
+export { Button, buttonVariants } from './components/ui/button/index.js';
 export type { ButtonEvents, ButtonProps, Events, Props } from './components/ui/button/index.js';
 
 export { Badge, badgeVariants } from './components/ui/badge/index.js';
 export type { Variant } from './components/ui/badge/index.js';
+
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from './components/ui/card/index.js';
+export type { HeadingLevel } from './components/ui/card/index.js';
