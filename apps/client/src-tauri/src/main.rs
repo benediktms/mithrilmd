e3cbdb6c87e57c88d449af26e3c8c6fab6d33914
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-18 09:12:32 +0100

    feat: port shakshuka

--- apps/client/src-tauri/src/main.rs
+++ apps/client/src-tauri/src/main.rs
@@ -1,13 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod startup;
 mod system_tray;
 mod util;
 
-use system_tray::{make_system_tray, system_tray_event_handler};
-use tauri::{LogicalSize, Manager, Size};
+use tauri::{LogicalSize, Manager, Size, WindowEvent};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
+
+use startup::startup;
+use system_tray::{make_system_tray, system_tray_event_handler};
 use util::load_from_env;
 
 #[tauri::command]
@@ -16,12 +19,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    let log_fmt = load_from_env::<String>("LOG_FORMAT").unwrap_or("default".into());
+    dotenvy::dotenv().ok();
+
+    let log_fmt = load_from_env::<String>("RUST_LOG").unwrap_or("default".into());
     let log_level = load_from_env::<LevelFilter>("LOG_LEVEL").unwrap_or(LevelFilter::TRACE);
 
     tracing_subscriber::registry()
         .with(if log_fmt == "json" {
-            fmt::layer().json().boxed()
+            fmt::layer()
+                .with_thread_ids(true)
+                .with_thread_names(true)
+                .json()
+                .boxed()
         } else if log_fmt == "pretty" {
             fmt::layer()
                 .with_thread_ids(true)
@@ -48,11 +57,19 @@ fn main() {
                 Err(err) => println!("{err}"),
                 Ok(res) => res,
             }
+            let _ = tauri::async_runtime::block_on(startup(app.handle()));
 
             Ok(())
         })
         .system_tray(make_system_tray())
         .on_system_tray_event(system_tray_event_handler())
+        .on_window_event(|e| {
+            let event = e.event();
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                e.window().hide().expect("could not hide window");
+                api.prevent_close();
+            }
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ crates/migration/src/m20220101_000001_create_vault_table.rs
@@ -0,0 +1,71 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let _ = manager
+            .create_table(
+                Table::create()
+                    .table(Vault::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Vault::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Vault::Path).string().not_null())
+                    .col(
+                        ColumnDef::new(Vault::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Vault::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .default(Expr::current_timestamp())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await;
+
+        let db = manager.get_connection();
+
+        db.execute_unprepared(
+            "CREATE TRIGGER vault_updated_at
+                AFTER
+                  UPDATE
+                ON vault
+                FOR EACH ROW
+            BEGIN
+                UPDATE vault
+                SET updated_at = CURRENT_TIMESTAMP
+                WHERE id = OLD.id;
+            END",
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Vault::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Vault {
+    Table,
+    Id,
+    Path,
+    CreatedAt,
+    UpdatedAt,
+}
