0b2911473918be1c9c226820138bf86c58794518
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-30 16:05:54 +0200

    fix: use correct dialog component

--- packages/shadcn-ui/src/lib/components/ui/command/command-dialog.svelte
+++ packages/shadcn-ui/src/lib/components/ui/command/command-dialog.svelte
@@ -11,7 +11,7 @@
   export let value: $$Props['value'] = undefined;
 </script>
 
-<Dialog bind:open {...$$restProps}>
+<Dialog.Root bind:open {...$$restProps}>
   <DialogContent class="overflow-hidden p-0 shadow-lg">
     <Command
       class="[&_[data-cmdk-group-heading]]:text-muted-foreground [&_[data-cmdk-group-heading]]:px-2 [&_[data-cmdk-group-heading]]:font-medium [&_[data-cmdk-group]:not([hidden])_~[data-cmdk-group]]:pt-0 [&_[data-cmdk-group]]:px-2 [&_[data-cmdk-input-wrapper]_svg]:h-5 [&_[data-cmdk-input-wrapper]_svg]:w-5 [&_[data-cmdk-input]]:h-12 [&_[data-cmdk-item]]:px-2 [&_[data-cmdk-item]]:py-3 [&_[data-cmdk-item]_svg]:h-5 [&_[data-cmdk-item]_svg]:w-5"
@@ -21,4 +21,4 @@
       <slot />
     </Command>
   </DialogContent>
-</Dialog>
+</Dialog.Root>
